0ae96e498612621356d5f5fabb7d9b4ef6cdd849
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-03-24 11:30:47 +0100

    concrete commands and events: remove comment

---
+++ Post.Command.API/Commands/RemoveCommentCommand.cs
@@ -0,0 +1,9 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Command.API.Commands;
+
+public class RemoveCommentCommand : BaseCommand
+{
+    public Guid CommentId { get; set; }
+    public string Username { get; set; }
+}
---
+++ Post.Common/Events/CommentRemovedEvent.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public class CommentRemovedEvent : BaseEvent
+{
+    public Guid CommentId { get; set; } 
+    public CommentRemovedEvent(string type) : base(nameof(CommentRemovedEvent))
+    {
+    }
+}
