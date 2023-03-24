94ecba5b8adc1ba37706f7b9bbc5d7e588fb15c2
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-03-24 11:31:29 +0100

    concrete commands and events: edit message

---
+++ Post.Command.API/Commands/EditMessageCommand.cs
@@ -0,0 +1,8 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Command.API.Commands;
+
+public class EditMessageCommand : BaseCommand
+{
+    public string Message { get; set; }
+}
---
+++ Post.Common/Events/MessageUpdatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public class MessageUpdatedEvent : BaseEvent
+{
+    public string Message { get; set; }
+    
+    public MessageUpdatedEvent(string type) : base(nameof(MessageUpdatedEvent))
+    {
+    }
+}
