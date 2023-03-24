ef8a8152ea886d9feecb7c2fe7caab74be8a8ad7
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-03-24 11:31:09 +0100

    concrete commands and events: remove post

---
+++ Post.Command.API/Commands/DeletePostCommand.cs
@@ -0,0 +1,8 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Command.API.Commands;
+
+public class DeletePostCommand : BaseCommand
+{
+    public string Username { get; set; }
+}
---
+++ Post.Common/Events/PostRemovedEvent.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events;
+
+public class PostRemovedEvent : BaseEvent
+{
+    public PostRemovedEvent(string type) : base(nameof(PostRemovedEvent))
+    {
+    }
+}
