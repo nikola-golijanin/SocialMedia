ed737255a4cfa7b94f38acb64ad98670f0367a7a
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-03-24 11:30:08 +0100

    concrete commands and events: Add comment

---
+++ Post.Command.API/Commands/AddCommentCommand.cs
@@ -0,0 +1,9 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Command.API.Commands;
+
+public abstract class AddCommentCommand : BaseCommand
+{
+    public string Comment { get; set; }
+    public string Username { get; set; }
+}
