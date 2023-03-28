4d70f775bdbcbf0125daf67c2cacf45af63a1806
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-03-28 13:20:06 +0200

    Event handling

---
+++ Post.Query.Infrastructure/Handlers/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using Post.Common.Events;
+
+namespace Post.Query.Infrastructure.Handlers;
+
+public interface IEventHandler
+{
+    Task On(PostCreatedEvent @event);
+    Task On(MessageUpdatedEvent @event);
+    Task On(PostLikedEvent @event);
+    Task On(CommentAddedEvent @event);
+    Task On(CommentRemovedEvent @event);
+    Task On(CommentUpdatedEvent @event);
+    Task On(PostRemovedEvent @event);
+}
