3e83354f22a25b6d3b9c5a4cb2f97d5d4c1873ba
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-03-29 14:58:29 +0200

    Added PostsController

---
+++ Post.Query.API/DTOs/PostLookupResponse.cs
@@ -0,0 +1,9 @@
+﻿using Post.Common.DTOs;
+using Post.Query.Domain.Entities;
+
+namespace Post.Query.API.DTOs;
+
+public class PostLookupResponse : BaseResponse
+{
+    public List<PostEntity> Posts { get; set; }
+}
